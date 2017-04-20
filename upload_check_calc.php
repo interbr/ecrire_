a35c130828f2b1266f76d7c9ebcc97098d199c19
Felix Longolius <felix@weltpolizei.de>
2017-04-21 00:28:45 +0200

    syntax correction

--- upload_check_calc.php
+++ upload_check_calc.php
@@ -33,10 +33,10 @@ if ( $_FILES['uploaddatei']['name']  <> "" )
 }
 }
 $nextcalc = (int)file_get_contents('calc/calc.txt');
-if $nextcalc = 1 { $newcode = (int)file_get_contents('code/code.txt') + 5; $newcalc = 2; }
-if $nextcalc = 2 { $newcode = (int)file_get_contents('code/code.txt') * 1.2; $newcalc = 3; }
-if $nextcalc = 3 { $newcode = (int)file_get_contents('code/code.txt') + 4; $newcalc = 4; }
-if $nextcalc = 4 { $newcode = (int)file_get_contents('code/code.txt') + 1; $newcalc = 1; }
+if $nextcalc = 1 ( $newcode = (int)file_get_contents('code/code.txt') + 5; $newcalc = 2; )
+if $nextcalc = 2 ( $newcode = (int)file_get_contents('code/code.txt') * 1.2; $newcalc = 3; )
+if $nextcalc = 3 ( $newcode = (int)file_get_contents('code/code.txt') + 4; $newcalc = 4; )
+if $nextcalc = 4 ( $newcode = (int)file_get_contents('code/code.txt') + 1; $newcalc = 1; )
 file_put_contents("code/code.txt", $newcode);
 file_put_contents("calc/calc.txt", $newcalc);
 ?>
