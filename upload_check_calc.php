cafad8d2dd932f1dd10fe79625162137c72d9ff8
Felix Longolius <felix@weltpolizei.de>
2017-04-20 23:42:14 +0200

    searching for operants

--- upload_check_calc.php
+++ upload_check_calc.php
@@ -3,7 +3,7 @@ echo "<pre>";
 echo "FILES:<br>";
 print_r ($_FILES );
 echo "</pre>";
-if ( $_FILES['uploaddatei']['name']  <> "" ) and ( (int)$_POST["number"] == (int)file_get_contents('code/code.txt'));
+if (( $_FILES['uploaddatei']['name']  <> "" ) and ( (int)$_POST["number"] == (int)file_get_contents('code/code.txt')));
 {
     // Datei wurde durch HTML-Formular hochgeladen
     // und kann nun weiterverarbeitet werden
