27172779d151fe3fb690e97e312f8e79ce3ef3da
Felix Longolius <felix@weltpolizei.de>
2017-05-02 16:43:13 +0200

    Translatet to french, remove announcement, display website

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 		<head>
-		<title>Write a novel to an end and make every 5437th upload visible on a website</title>
+		<title>Ecrire une histoire courte ou un roman et télécharger le document dans cette site du web</title>
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="description" content="Write a novel to an end and make every 5437th upload visible on a website">
@@ -9,20 +9,18 @@
 		</head>
 		<body>
 		<center>
-		<h1>This system is starting up on tuesday, 28 years after Hungary opened its borders, 14:07h NPT!</h1>
-		<p>Ok, let's make it today, 20:15 NPT, d'accord?</p><!--
-		<p><a href="/der-planet-der-sechs-loeffel.odt">Der Text (Den Beginn bitte nicht &auml;ndern)</a></p>
+		<p><a href="/der-planet-der-sechs-loeffel.odt">Le text (S'il vous plait ne changer le introduction).</a></p>
 		<form enctype="multipart/form-data" action="upload_check_calc.php" method="post">
-		<p>Diese Datei hochladen: <input name="uploaddatei" type="file" /></p>
-		<p>Upload-Number: <input type="text" name="number" size="60" maxlength="255" /></p>
+		<p>Télécharge cette document: <input name="uploaddatei" type="file" /></p>
+		<p>Avec le code: <input type="text" name="number" size="60" maxlength="255" /></p>
 		<p><input type="submit" value="Send File" /></p>
 		</form>
-		<p>The latest 67 Files uploaded with the same code as the code calculated the code being a 5437th correct code:</p>
+		<p>Les derniere 67 document télécharger avec le même code comme code.txt:</p>
 		<?php 
 			foreach (glob("hochgeladenes_public/*.*") as $filename) {
 				echo '<a href="'.$filename.'">'.$filename.'</a><br />';
 			}
-		?>-->
+		?>
 		</center>
 		<p style="float: right;"><a href="https://github.com/interbr/ecrire_">https://github.com/interbr/ecrire_</a></p>
 		</body>
